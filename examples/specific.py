7c8b1aa836c158ea9b961163940d3d7ef914cf00
JHolah <2090181+JHolah@users.noreply.github.com>
2021-02-12 17:30:07 +0000

    Update specific.py

--- examples/specific.py
+++ examples/specific.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pms5003 import PMS5003
+from pms7003 import PMS7003
 
 print("""specific.py - Continously print a specific data value.
 
@@ -8,8 +8,8 @@ Press Ctrl+C to exit!
 
 """)
 
-# Configure the PMS5003 for Enviro+
-pms5003 = PMS5003(
+# Configure the pms7003 for Enviro+
+pms5003 = pms7003(
     device='/dev/ttyAMA0',
     baudrate=9600,
     pin_enable=22,
@@ -18,7 +18,7 @@ pms5003 = PMS5003(
 
 try:
     while True:
-        data = pms5003.read()
+        data = pms7003.read()
         print("PM2.5 ug/m3 (combustion particles, organic compounds, metals): {}".format(data.pm_ug_per_m3(2.5)))
 
 except KeyboardInterrupt:
